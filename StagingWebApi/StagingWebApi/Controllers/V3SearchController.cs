d6c611bd51aa90223b7f301f221ce85939677452
johnataylor <johtaylo@microsoft.com>
2015-10-01 17:09:25 -0700

    added v3 search pass thru

---
+++ StagingWebApi/StagingWebApi/Controllers/V3SearchController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using StagingWebApi.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StagingWebApi.Controllers
+{
+    public class V3SearchController : ApiController
+    {
+        [Route("v3/query/{owner}/{name}")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Query(string owner, string name)
+        {
+            V3Resource resource = new V3Resource(owner, name);
+
+            Uri searchAddress = await resource.GetSearchQueryService();
+            if (searchAddress == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            string query = Request.RequestUri.Query;
+
+            Uri searchQuery = new Uri(searchAddress.AbsoluteUri + query);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage searchResponse = await client.GetAsync(searchQuery);
+
+            string json = await searchResponse.Content.ReadAsStringAsync();
+
+            JObject obj = JObject.Parse(json);
+
+            //  Dynamically merge the results as we pass them on through...
+            //  (1) load local lucene
+            //  (2) add local package metadata
+            //  (3) query local lucene
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = Utils.CreateJsonContent(obj.ToString());
+
+            return response;
+        }
+    }
+}
---
+++ StagingWebApi/StagingWebApi/Resources/V3Resource.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace StagingWebApi.Resources
+{
+    public class V3Resource : StageResourceBase
+    {
+        public V3Resource(string ownerName, string stageId)
+            : base(ownerName, stageId)
+        {
+            Configuration rootWebConfig = WebConfigurationManager.OpenWebConfiguration("/StagingWebApi");
+            ConnectionString = rootWebConfig.ConnectionStrings.ConnectionStrings["PackageStaging"].ConnectionString;
+        }
+
+        public string ConnectionString
+        {
+            get;
+            set;
+        }
+
+        public async Task<Uri> GetPackageBaseAddress()
+        {
+            Uri serviceBase = await GetServiceBase();
+            if (serviceBase == null)
+            {
+                return null;
+            }
+
+            Uri address = await Utils.GetService(serviceBase, "PackageBaseAddress/3.0.0");
+            return (address == null) ? null : address;
+        }
+
+        public async Task<Uri> GetSearchQueryService()
+        {
+            Uri serviceBase = await GetServiceBase();
+            if (serviceBase == null)
+            {
+                return null;
+            }
+
+            Uri address = await Utils.GetService(serviceBase, "SearchQueryService/3.0.0-beta");
+            return (address == null) ? null : address;
+        }
+
+        async Task<Uri> GetServiceBase()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(@"
+                    SELECT Stage.BaseService
+                    FROM Stage
+                    INNER JOIN StageOwner ON Stage.[Key] = StageOwner.StageKey
+                    INNER JOIN Owner ON Owner.[Key] = StageOwner.OwnerKey
+                    WHERE Owner.Name = @OwnerName
+                      AND Stage.Name = @StageName
+                ", connection);
+
+                command.Parameters.AddWithValue("OwnerName", OwnerName);
+                command.Parameters.AddWithValue("StageName", StageName);
+
+                string result = (string)await command.ExecuteScalarAsync();
+
+                if (result == null)
+                {
+                    return null;
+                }
+
+                return new Uri(result);
+            }
+        }
+    }
+}
