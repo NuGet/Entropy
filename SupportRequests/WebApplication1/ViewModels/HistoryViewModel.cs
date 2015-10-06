e4de8be27f06ed054dc9d29d2725d14f33468087
RanjiniM <ranjinim@microsoft.com>
2015-10-06 13:25:12 -0700

    Support Request Prototype

---
+++ SupportRequests/WebApplication1/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class HistoryViewModel
+    {
+        public History History { get; set; }
+        public string EditedBy { get; set; }
+    }
+}
