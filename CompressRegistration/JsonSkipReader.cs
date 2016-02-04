83f76194ff05455ffbbac5c47f1e70725b0e2dfd
johnataylor <johtaylo@microsoft.com>
2016-02-04 12:01:12 -0800

    compressing registration blobs

---
+++ CompressRegistration/JsonSkipReader.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressRegistration
+{
+    class JsonSkipReader : JsonReader
+    {
+        JsonReader _innerReader;
+
+        HashSet<string> _properties;
+
+        string _propertyName;
+        string _baseAddress;
+
+        public JsonSkipReader(JsonReader innerReader)
+        {
+            _innerReader = innerReader;
+            _innerReader.DateParseHandling = DateParseHandling.None;
+
+            _properties = new HashSet<string>();
+            
+            // full strip
+
+            _properties.Add("authors");
+            _properties.Add("description");
+            _properties.Add("iconUrl");
+            _properties.Add("language");
+            _properties.Add("licenseUrl");
+            _properties.Add("minClientVersion");
+            _properties.Add("projectUrl");
+            _properties.Add("published");
+            _properties.Add("requireLicenseAcceptance");
+            _properties.Add("summary");
+            _properties.Add("title");
+            _properties.Add("tags");
+            _properties.Add("commitId");
+            _properties.Add("commitTimeStamp");
+            _properties.Add("packageContent");
+            _properties.Add("registration");
+            _properties.Add("@type");
+            _properties.Add("@context");
+
+            // partial strip
+
+            //_properties.Add("authors");
+            //_properties.Add("iconUrl");
+            //_properties.Add("language");
+            //_properties.Add("minClientVersion");
+            //_properties.Add("summary");
+            //_properties.Add("title");
+            //_properties.Add("commitId");
+            //_properties.Add("commitTimeStamp");
+            //_properties.Add("packageContent");
+            //_properties.Add("registration");
+            //_properties.Add("@type");
+            //_properties.Add("@context");
+
+            _propertyName = string.Empty;
+            _baseAddress = "https://api.nuget.org/v3/";
+        }
+
+        public override bool Read()
+        {
+            if (!_innerReader.Read())
+            {
+                return false;
+            }
+
+            if (_innerReader.TokenType == JsonToken.PropertyName)
+            {
+                _propertyName = (string)_innerReader.Value;
+            }
+
+            if (_innerReader.TokenType == JsonToken.PropertyName && _properties.Contains((string)_innerReader.Value))
+            {
+                _innerReader.Skip();
+
+                return Read();
+            }
+
+            return true;
+        }
+
+        public override object Value
+        {
+            get
+            {
+                if (_innerReader.TokenType == JsonToken.String && _propertyName == "@id")
+                {
+                    string address = (string)_innerReader.Value;
+                    if (address.StartsWith(_baseAddress))
+                    {
+                        return address.Substring(_baseAddress.Length);
+                    }
+                }
+
+                return _innerReader.Value;
+            }
+        }
+
+        public override JsonToken TokenType
+        {
+            get
+            {
+                return _innerReader.TokenType;
+            }
+        }
+
+        public override string Path
+        {
+            get
+            {
+                return _innerReader.Path;
+            }
+        }
+
+        public override int Depth
+        {
+            get
+            {
+                return _innerReader.Depth;
+            }
+        }
+
+        public override Type ValueType
+        {
+            get
+            {
+                return _innerReader.ValueType;
+            }
+        }
+    }
+}
---
+++ CompressRegistration/Program.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CompressRegistration
+{
+    class Program
+    {
+        static async Task Download(string id, string path)
+        {
+            var index = new Uri(string.Format("https://api.nuget.org/v3/registration1/{0}/index.json", id.ToLowerInvariant()));
+
+            HttpClient client = new HttpClient();
+
+            string jsonIndex = await client.GetStringAsync(index);
+            JObject objIndex = JObject.Parse(jsonIndex);
+
+            File.WriteAllText(path + "index.json", jsonIndex);
+
+            foreach (var item in objIndex["items"])
+            {
+                string pageUri = item["@id"].ToString();
+
+                Console.WriteLine("{0}", pageUri);
+
+                string jsonPage = await client.GetStringAsync(pageUri);
+                JObject jsonObj = JObject.Parse(jsonPage);
+
+                var lower = item["lower"];
+                var upper = item["upper"];
+
+                string filename = string.Format("{0}_{1}_{2}.json", id.ToLowerInvariant(), lower, upper);
+
+                File.WriteAllText(path + filename, jsonPage);
+            }
+        }
+
+        static void Strip(string id, string path)
+        {
+            var directoryInfo = new DirectoryInfo(path);
+
+            foreach (var fileInfo in directoryInfo.EnumerateFiles(id.ToLowerInvariant() + "_*.json"))
+            {
+                using (FileStream stream = fileInfo.OpenRead())
+                {
+                    using (var textReader = new StreamReader(stream))
+                    {
+                        using (var jsonReader = new JsonSkipReader(new JsonTextReader(textReader)))
+                        {
+                            JObject obj = JObject.Load(jsonReader);
+                            foreach (var item in obj["items"])
+                            {
+                                JToken catalogEntry = item["catalogEntry"];
+                                Console.WriteLine("{0}/{1}", catalogEntry["id"], catalogEntry["version"]);
+                            }
+                            File.WriteAllText(fileInfo.FullName + ".txt", obj.ToString(Formatting.None));
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Inline(string id, string path)
+        {
+            //  step (1) create a dictionary of all the JSON objects key by the resource @id they describe 
+
+            var lookup = new Dictionary<string, JObject>();
+
+            var directoryInfo = new DirectoryInfo(path);
+            foreach (var fileInfo in directoryInfo.EnumerateFiles(id.ToLowerInvariant() + "_*.json"))
+            {
+                using (FileStream stream = fileInfo.OpenRead())
+                {
+                    using (var textReader = new StreamReader(stream))
+                    {
+                        using (var jsonReader =new JsonTextReader(textReader))
+                        {
+                            JObject objItem = JObject.Load(jsonReader);
+                            string resourceId = objItem["@id"].ToString();
+                            lookup[resourceId] = objItem;
+                        }
+                    }
+                }
+            }
+
+            //  step (2) now inline these into a newly created items array and replace the current array
+
+            string jsonIndex = File.ReadAllText(path + "index.json");
+            JObject objIndex = JObject.Parse(jsonIndex);
+
+            JArray inlinedPages = new JArray();
+
+            foreach (var page in objIndex["items"])
+            {
+                string resourceId = page["@id"].ToString();
+                JObject objItem = lookup[resourceId];
+                JToken newPage = page.DeepClone();
+                newPage["items"] = objItem["items"];
+                inlinedPages.Add(newPage);
+            }
+
+            objIndex["items"] = inlinedPages;
+
+            //  step (3) finally dump out the newly minted JSON
+
+            File.WriteAllText(path + "inline.json", objIndex.ToString());
+            File.WriteAllText(path + "inlineMinified.json", objIndex.ToString(Formatting.None));
+        }
+
+        static void Test0()
+        {
+            const string Id = "ravendb.client";
+            const string Path = @"c:\data\scratch\";
+            Download(Id, Path).Wait();
+            Strip(Id, Path);
+        }
+
+        static void Test1()
+        {
+            const string Id = "ravendb.client";
+            const string Path = @"c:\data\scratch\";
+            Download(Id, Path).Wait();
+            Inline(Id, Path);
+
+            var fileInfo = new FileInfo(Path + "inline.json");
+
+            using (var stream = fileInfo.OpenRead())
+            {
+                using (var textReader = new StreamReader(stream))
+                {
+                    using (var jsonReader = new JsonSkipReader(new JsonTextReader(textReader)))
+                    {
+                        JObject obj = JObject.Load(jsonReader);
+
+                        File.WriteAllText(Path + "inlineSkipped.json", obj.ToString());
+                        File.WriteAllText(Path + "inlineSkippedAndMinified.json", obj.ToString(Formatting.None));
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Test0();
+                Test1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
