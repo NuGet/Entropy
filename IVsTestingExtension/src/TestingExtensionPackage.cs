03c83d1e18c2650fc18ffff04c6a61e8cfd3348c
Nikolche Kolev <nikolce1kolev@gmail.com>
2019-12-18 11:13:50 -0800

    Add an IVsTestingExtension (#21)

---
+++ IVsTestingExtension/src/TestingExtensionPackage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using IVsTestingExtension.Tests;
+using IVsTestingExtension.ToolWindows;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace IVsTestingExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("IVs Testing Extension", "Helps test the IVs APIs in Visual Studio by invoking on different threading contexts.", "1.0")]
+    [ProvideToolWindow(typeof(CommandInvokingWindow), Style = VsDockStyle.Tabbed, DockedWidth = 300, Window = "DocumentWell", Orientation = ToolWindowOrientation.Left)]
+    [Guid("5A86D30C-B4EC-4537-8B81-A422058075CE")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class TestingExtensionPackage : AsyncPackage
+    {
+        [Import]
+        ITestMethodProvider TestMethodFactory { get; set; }
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            var componentModel = await this.GetComponentModelAsync();
+            componentModel.DefaultCompositionService.SatisfyImportsOnce(this);
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+            await ShowToolWindow.InitializeAsync(this);
+        }
+
+        public override IVsAsyncToolWindowFactory GetAsyncToolWindowFactory(Guid toolWindowType)
+        {
+            return toolWindowType.Equals(Guid.Parse(CommandInvokingWindow.WindowGuidString)) ? this : null;
+        }
+
+        protected override string GetToolWindowTitle(Type toolWindowType, int id)
+        {
+            return toolWindowType == typeof(CommandInvokingWindow) ? CommandInvokingWindow.Title : base.GetToolWindowTitle(toolWindowType, id);
+        }
+
+        protected override async Task<object> InitializeToolWindowAsync(Type toolWindowType, int id, CancellationToken cancellationToken)
+        {
+            var dte = await this.GetDTEAsync();
+            var model = new ProjectCommandTestingModel(dte, TestMethodFactory.GetMethod());
+            await model.InitializeAsync();
+            return model;
+        }
+    }
+}
---
+++ IVsTestingExtension/src/ToolWindows/CommandInvokingWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Shell;
+
+namespace IVsTestingExtension.ToolWindows
+{
+    [Guid(WindowGuidString)]
+    public class CommandInvokingWindow : ToolWindowPane
+    {
+        public const string WindowGuidString = "5D42A8E4-F0AE-44BD-B0A1-193C8901364C";
+        public const string Title = "Command Invoking Window";
+
+        public CommandInvokingWindow(ProjectCommandTestingModel state) : base()
+        {
+            Caption = Title;
+            BitmapImageMoniker = KnownMonikers.ImageIcon;
+            Content = new ToolWindowControl(state);
+        }
+    }
+}
