87c41b8a6d701a90f68f621168aecb345efd0439
Nikolche Kolev <nikolev@microsoft.com>
2020-03-19 12:51:29 -0700

    cleanup

---
+++ GithubIssueTagger/IssueUtilities.cs
@@ -0,0 +1,66 @@
+﻿using Octokit;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubIssueTagger
+{
+    internal class IssueUtilities
+    {
+        private static DateTimeOffset SixMonthsFromAppStartup = new DateTimeOffset(DateTime.Now.AddDays(-180.0));
+
+        public static bool HasTestLabel(Issue issue)
+        {
+            return issue.State == ItemState.Open && issue.Labels.Any(e => e.Name.Equals("Area:Test"));
+        }
+
+        public static async Task AddLabelToMatchingIssues(GitHubClient client, string label, string org, string repo, Predicate<Issue> predicate)
+        {
+            var issuesForRepo = await client.Issue.GetAllForRepository(org, repo);
+
+            foreach (var issue in issuesForRepo)
+            {
+                if (predicate(issue))
+                {
+                    try
+                    {
+                        var issueUpdate = issue.ToUpdate();
+                        issueUpdate.AddLabel(label);
+                        await client.Issue.Update(org, repo, issue.Number, issueUpdate);
+                        Console.WriteLine($"Updated issue: {issue.HtmlUrl}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Unhandled issue {issue.HtmlUrl} {e}");
+                    }
+                }
+            }
+        }
+
+        public static async Task RemoveLabelFromAllIssuesAsync(GitHubClient client, string label, string org, string repo)
+        {
+            var issuesForRepo = await client.Issue.GetAllForRepository(org, repo);
+
+            foreach (var issue in issuesForRepo)
+            {
+                if (issue.Labels != null)
+                {
+                    if (issue.Labels.Any(e => e.Name.Equals(label)))
+                    {
+                        try
+                        {
+                            var issueUpdate = issue.ToUpdate();
+                            issueUpdate.RemoveLabel(label);
+                            await client.Issue.Update(org, repo, issue.Number, issueUpdate);
+                            Console.WriteLine($"Updated issue: {issue.HtmlUrl}");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Unhandled issue {issue.HtmlUrl} {e}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- GithubIssueTagger/Program.cs
+++ GithubIssueTagger/Program.cs
@@ -1,85 +1,15 @@
 ﻿using Octokit;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GithubIssueTagger
 {
-    class Program
+    public class Program
     {
-        private const string token = "YourGithubToken";
-        private static DateTimeOffset SixMonthsFromAppStartup = new DateTimeOffset(DateTime.Now.AddDays(-180.0));
-
         static async Task Main(string[] args)
         {
             var client = new GitHubClient(new ProductHeaderValue("nuget-github-issue-tagger"));
-            client.Credentials = new Credentials(GetClientSecret());
-
-            await RemoveLabelFromAllIssuesAsync(client, "ToBeMoved", "nuget", "home");
-
-            Predicate<Issue> predicate = WasUpdatedInLastSixMonths;
-            await AddLabelToMatchingIssues(client, "ToBeMoved", "nuget", "home", predicate);
-
-            Console.WriteLine("Done!");
-        }
-
-        private static string GetClientSecret()
-        {
-            return token;
-        }
-
-        private static bool WasUpdatedInLastSixMonths(Issue issue)
-        {
-            return issue.State == ItemState.Open && issue.UpdatedAt.Value.DateTime < SixMonthsFromAppStartup;
-        }
-
-        private static async Task AddLabelToMatchingIssues(GitHubClient client, string label, string org, string repo, Predicate<Issue> predicate)
-        {
-            var issuesForRepo = await client.Issue.GetAllForRepository(org, repo);
-
-            foreach (var issue in issuesForRepo)
-            {
-                if (predicate(issue))
-                {
-                    try
-                    {
-                        var issueUpdate = issue.ToUpdate();
-                        issueUpdate.AddLabel(label);
-                        await client.Issue.Update(org, repo, issue.Number, issueUpdate);
-                        Console.WriteLine($"Updated issue: {issue.HtmlUrl}");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Unhandled issue {issue.HtmlUrl} {e}");
-                    }
-                }
-            }
-        }
-
-        private static async Task RemoveLabelFromAllIssuesAsync(GitHubClient client, string label, string org, string repo)
-        {
-            var issuesForRepo = await client.Issue.GetAllForRepository(org, repo);
-
-            foreach (var issue in issuesForRepo)
-            {
-                if (issue.Labels != null)
-                {
-                    if (issue.Labels.Any(e => e.Name.Equals(label)))
-                    {
-                        try
-                        {
-                            var issueUpdate = issue.ToUpdate();
-                            issueUpdate.RemoveLabel(label);
-                            await client.Issue.Update(org, repo, issue.Number, issueUpdate);
-                            Console.WriteLine($"Updated issue: {issue.HtmlUrl}");
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine($"Unhandled issue {issue.HtmlUrl} {e}");
-                        }
-                    }
-                }
-            }
+            client.Credentials = new Credentials(args[0]);
+            await PullRequestUtilities.ProcessPullRequestStatsAsync(client);
         }
     }
 }
