c5c9360fd507067eb3c49eca2b5103198848c93b
Nikolche Kolev <nikolev@microsoft.com>
2020-03-19 14:21:04 -0700

    address feedback

--- GithubIssueTagger/Program.cs
+++ GithubIssueTagger/Program.cs
@@ -7,6 +7,11 @@ namespace GithubIssueTagger
     {
         static async Task Main(string[] args)
         {
+            if (args.count != 1)
+            {
+                Console.Err.WriteLine("Expected 1 argument (github PAT). Found " + args.Count);
+                return;
+            }
             var client = new GitHubClient(new ProductHeaderValue("nuget-github-issue-tagger"));
             client.Credentials = new Credentials(args[0]);
             await PullRequestUtilities.ProcessPullRequestStatsAsync(client);
