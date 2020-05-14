356997dad50cc5624e96b4cf4037503c8ef3a955
Joel Verhagen <jver@microsoft.com>
2020-05-14 14:58:32 -0700

    Add package downloader

---
+++ PackageDownloader/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+
+namespace PackageDownloader
+{
+    class Program
+    {
+        private const string RootMarker = "discover-packages.ps1";
+
+        static async Task<int> Main(string[] args)
+        {
+            if (!TryFindRoot(out var rootDir))
+            {
+                return 1;
+            }
+
+            // Read IDs from *-ids.txt
+            var ids = Directory
+                .EnumerateFiles(rootDir, "*-ids.txt")
+                .SelectMany(filePath => File.ReadAllLines(filePath))
+                .Distinct(StringComparer.OrdinalIgnoreCase)
+                .OrderBy(x => x, StringComparer.OrdinalIgnoreCase)
+                .ToList();
+            Console.WriteLine($"Found {ids.Count} package IDs in the log files.");
+
+            var sourceRepository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+            var resource = await sourceRepository.GetResourceAsync<FindPackageByIdResource>();
+
+            var idBag = new ConcurrentBag<string>(ids);
+            var idVersionBag = new ConcurrentBag<PackageIdentity>();
+
+            // Download all of the versions of every package ID
+            var workers = Enumerable
+                .Range(0, 32)
+                .Select(async i =>
+                {
+                    while (idBag.Count > 0 || idVersionBag.Count > 0)
+                    {
+                        while (idBag.TryTake(out var id))
+                        {
+                            using (var cacheContext = new SourceCacheContext())
+                            {
+                                Console.WriteLine($"[{i,2}] Getting version list for {id}...");
+                                var versions = await resource.GetAllVersionsAsync(id, cacheContext, NullLogger.Instance, CancellationToken.None);
+                                foreach (var version in versions)
+                                {
+                                    idVersionBag.Add(new PackageIdentity(id, version));
+                                }
+                            }
+                        }
+
+                        while (idVersionBag.TryTake(out var identity))
+                        {
+                            using (var cacheContext = new SourceCacheContext())
+                            {
+                                var path = Path.Combine(rootDir, "nupkgs", $"{identity.Id.ToLowerInvariant()}.{identity.Version.ToNormalizedString().ToLowerInvariant()}.nupkg");
+                                if (File.Exists(path))
+                                {
+                                    continue;
+                                }
+                                Console.WriteLine($"[{i,2}] Downloading {identity.Id} {identity.Version.ToNormalizedString()}...");
+                                var downloader = await resource.GetPackageDownloaderAsync(identity, cacheContext, NullLogger.Instance, CancellationToken.None);
+                                await downloader.CopyNupkgFileToAsync($"{path}.download", CancellationToken.None);
+                                File.Move($"{path}.download", path);
+                            }
+                        }
+                    }
+                })
+                .ToList();
+
+            await Task.WhenAll(workers);
+
+            return 0;
+        }
+
+        static bool TryFindRoot(out string dir)
+        {
+            dir = Directory.GetCurrentDirectory();
+            var tried = new List<string>();
+            bool foundRoot;
+            do
+            {
+                tried.Add(dir);
+                foundRoot = Directory
+                    .EnumerateFiles(dir)
+                    .Select(filePath => Path.GetFileName(filePath))
+                    .Contains(RootMarker);
+
+                if (!foundRoot)
+                {
+                    dir = Path.GetDirectoryName(dir);
+                }
+            }
+            while (dir != null && !foundRoot);
+
+            if (!foundRoot)
+            {
+                Console.WriteLine($"Could not find {RootMarker} in the current or parent directories.");
+                Console.WriteLine("Tried:");
+                foreach (var path in tried)
+                {
+                    Console.WriteLine($"  {path}");
+                }
+
+                dir = null;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- discover-packages.ps1
+++ discover-packages.ps1
@@ -1,65 +1,94 @@
+$ErrorActionPreference = "Stop"
+
 $testDir = "$PSScriptRoot\perftests\testCases"
 $nugetUrl = "https://dist.nuget.org/win-x86-commandline/v5.5.1/nuget.exe"
 $nugetPath = "$PSScriptRoot\nuget.exe"
 
-if (!(Test-Path $nugetPath)) {
-    $progressPreference = 'silentlyContinue'
-    try {
+$progressPreference = "silentlyContinue"
+try {
+    if (!(Test-Path $nugetPath)) {
         Invoke-WebRequest $nugetUrl -OutFile $nugetPath
-    } finally {
-        $progressPreference = 'Continue'
+    }
+
+    $testCases = Get-ChildItem "$testDir\Test-*.ps1"
+    $testOutputDir = "$PSScriptRoot\work"
+    $nupkgOutputDir = "$PSScriptRoot\nupkgs"
+
+    Remove-Item -Path "$nupkgOutputDir\*.download" | Out-Null
+    Remove-Item -Path "$PSScriptRoot\*-ids.txt" | Out-Null
+
+    foreach ($path in $testCases) {
+        $testName = $path.Name.Split("-", 2)[1].Split(".", 2)[0]
+        $outDir = "$testOutputDir\$testName"
+
+        Remove-Item -Path $outDir\logs\*.txt | Out-Null
+
+        & $path `
+            -nugetClientFilePath $nugetPath `
+            -sourceRootFolderPath $outDir\source `
+            -resultsFolderPath $outDir\results  `
+            -logsFolderPath $outDir\logs `
+            -nugetFoldersPath $outDir\nuget `
+            -dumpNupkgsPath $nupkgOutputDir `
+            -iterationCount 1 `
+            -skipCleanRestores `
+            -skipColdRestores `
+            -skipForceRestores `
+            -skipNoOpRestores
+
+        Get-ChildItem $outDir\logs\*.txt `
+            | ForEach-Object { Get-Content $_ } `
+            | Where-Object { $_.StartsWith("  GET https://api.nuget.org/v3-flatcontainer/") } `
+            | ForEach-Object { if ($_ -match "v3-flatcontainer/([^/]+)") { $Matches[1] } } `
+            | Sort-Object `
+            | Get-Unique `
+            > $PSScriptRoot\$testName-ids.txt
+    }
+
+    # Determine all of the IDs from the logs.
+    Write-Host "Finding the IDs of HTTPs request in logs..."
+    $logIds = Get-ChildItem $PSScriptRoot\*-ids.txt `
+        | ForEach-Object { Get-Content $_ } `
+        | Sort-Object `
+        | Get-Unique
+
+    # Download all versions of all package IDs from nuget.org.
+    $serviceIndex = Invoke-RestMethod "https://api.nuget.org/v3/index.json"
+    $packageBaseAddress = $serviceIndex.resources `
+        | Where-Object { ($_ | Select-Object -ExpandProperty "@type") -eq "PackageBaseAddress/3.0.0" } `
+        | ForEach-Object { $_ | Select-Object -ExpandProperty "@id" }
+    $packageBaseAddress = $packageBaseAddress.TrimEnd("/")
+
+    foreach ($id in $logIds) {
+        Write-Host "Downloading all versions of $id..."
+        $lowerId = $id.ToLowerInvariant()
+        $versionListUrl = "$packageBaseAddress/$lowerId/index.json"
+        try {
+
+            $versionList = Invoke-RestMethod $versionListUrl
+        }
+        catch {
+            if ($ex.Exception.Response.StatusCode.value__) {
+                continue;
+            }
+        }
+        Write-Host "Found $($versionList.versions.Count) versions."
+        foreach ($version in $versionList.versions) {
+            $lowerVersion = $version.ToLowerInvariant()
+            $fileName = "$lowerId.$lowerVersion.nupkg"
+            $nupkgPath = "$PSScriptRoot\nupkgs\$fileName"
+
+            if (Test-Path $nupkgPath) {
+                continue;
+            }
+
+            Write-Host "Downloading version $version..."
+            $nupkgUrl = "$packageBaseAddress/$lowerId/$lowerVersion/$fileName"
+            Invoke-WebRequest $nupkgUrl -OutFile "$nupkgPath.download"
+            Move-Item "$nupkgPath.download" $nupkgPath
+        }
     }
 }
-
-$testCases = Get-ChildItem "$testDir\Test-*.ps1"
-$testOutputDir = "$PSScriptRoot\work"
-$nupkgOutputDir = "$PSScriptRoot\nupkgs"
-
-Remove-Item -Path $nupkgOutputDir -Force -Recurse | Out-Null
-Remove-Item -Path "$PSScriptRoot\*-ids.txt" | Out-Null
-
-foreach ($path in $testCases) {
-    $testName = $path.Name.Split("-", 2)[1].Split(".", 2)[0]
-    $outDir = "$testOutputDir\$testName"
-
-    Remove-Item -Path $outDir\logs\*.txt | Out-Null
-
-    & $path `
-        -nugetClientFilePath $nugetPath `
-        -sourceRootFolderPath $outDir\source `
-        -resultsFolderPath $outDir\results  `
-        -logsFolderPath $outDir\logs `
-        -nugetFoldersPath $outDir\nuget `
-        -dumpNupkgsPath $nupkgOutputDir `
-        -iterationCount 1 `
-        -skipCleanRestores `
-        -skipColdRestores `
-        -skipForceRestores `
-        -skipNoOpRestores
-
-    Get-ChildItem $outDir\logs\*.txt `
-        | ForEach-Object { Get-Content $_ } `
-        | Where-Object { $_.StartsWith("  GET https://api.nuget.org/v3-flatcontainer/") } `
-        | ForEach-Object { if ($_ -match "v3-flatcontainer/([^/]+)") { $Matches[1] } } `
-        | Sort-Object `
-        | Get-Unique `
-        > $PSScriptRoot\$testName-ids.txt
-}
-
-# Determine all of the IDs from the .nupkg dump.
-$nupkgIds = & $nugetPath list -source $nupkgOutputDir -Prerelease `
-    | ForEach-Object { $_.Split(' ')[0].ToLowerInvariant() } `
-    | Sort-Object
-
-# Determine all of the IDs from the logs.
-$logIds = Get-ChildItem $PSScriptRoot\*-ids.txt `
-    | ForEach-Object { Get-Content $_ } `
-    | Sort-Object `
-    | Get-Unique
-
-$inNupkgsButNotLogs = $nupkgIds | Where-Object { $_ -notin $logIds }
-$inLogsButNotNupkgs = $logsIds | Where-Object { $_ -notin $nupkgIds }
-if ($inNupkgsButNotLogs -or $inLogsButNotNupkgs) {
-    Write-Error "In .nupkg dump but not logs: $inNupkgsButNotLogs"
-    Write-Error "In logs but not .nupkg dump: $inLogsButNotNupkgs"
+finally {
+    $progressPreference = "Continue"
 }
