ce53febd984602d542d1f857628c7bd140cb8fee
Joel Verhagen <jver@microsoft.com>
2020-05-16 10:13:52 -0700

    Fix up discover script and add run tests script

--- run-tests.ps1
+++ run-tests.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop"
 
 $testDir = "$PSScriptRoot\scripts\perftests\testCases"
 $nugetPath = "$PSScriptRoot\nuget.exe"
-$packageHelper = "$PSScriptRoot\src\PackageHelper\PackageHelper.csproj"
 $testCases = Get-ChildItem "$testDir\Test-*.ps1"
 
 # Download NuGet, if it does not exist yet.
@@ -15,15 +14,10 @@ foreach ($path in $testCases) {
         -nugetClientFilePath $nugetPath `
         -resultsFilePath $PSScriptRoot\out\results.csv  `
         -logsFolderPath $PSScriptRoot\out\logs `
-        -dumpNupkgsPath $PSScriptRoot\out\nupkgs `
         -sourceRootFolderPath $PSScriptRoot\out\_s `
         -nugetFoldersPath $PSScriptRoot\out\_t `
-        -iterationCount 1 `
-        -skipCleanRestores `
+        -iterationCount 5 `
         -skipColdRestores `
         -skipForceRestores `
         -skipNoOpRestores
 }
-
-# Download all versions of all .nupkgs
-dotnet run download-packages --project $packageHelper
