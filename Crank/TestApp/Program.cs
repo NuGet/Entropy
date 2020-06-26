8f55e7e347b69fa1c0d6304dbc3c6cc8d889463e
Joel Verhagen <jver@microsoft.com>
2020-06-25 18:07:56 -0700

    Add test app

---
+++ Crank/TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || !int.TryParse(args[0], out var sleepDuration))
+            {
+                sleepDuration = 1000;
+            }
+
+            Console.WriteLine($"Waiting for {sleepDuration}ms...");
+            Thread.Sleep(sleepDuration);
+        }
+    }
+}
