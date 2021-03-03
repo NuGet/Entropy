056f13f1afbf73cc4ad1c64031da0a41904e260b
Donnie Goodson <donnie.goodson@microsoft.com>
2021-03-03 13:16:11 -0500

    Show untyped Issues per area

--- GithubIssueTagger/Program.cs
+++ GithubIssueTagger/Program.cs
@@ -148,13 +148,13 @@ namespace GithubIssueTagger
             {
                 //2671458320      Type:Tracking
                 _allLabels.SingleOrDefault(label => label.Id == 2671458320),
-                
+
                 //801160517       Type: Spec
                 _allLabels.SingleOrDefault(label => label.Id == 801160517),
 
                 //1593926950      Type: DeveloperDocs
                 _allLabels.SingleOrDefault(label => label.Id == 1593926950),
-                
+
                 //249737088       Type: Docs
                 _allLabels.SingleOrDefault(label => label.Id == 249737088),
 
@@ -163,11 +163,21 @@ namespace GithubIssueTagger
 
                 //2185215650      Type: Learning
                 _allLabels.SingleOrDefault(label => label.Id == 2185215650),
+            };
 
+            List<Label> validTypeLabels = new List<Label>()
+            {
                 //180116450       Type:Bug
+                _allLabels.SingleOrDefault(label => label.Id == 180116450),
+
                 //386656158       Type: DataAnalysis
+                _allLabels.SingleOrDefault(label => label.Id == 386656158),
+
                 //180970997       Type: DCR
+                _allLabels.SingleOrDefault(label => label.Id == 180970997),
+
                 //979424473       Type: Test
+                _allLabels.SingleOrDefault(label => label.Id == 979424473),
             };
 
             IEnumerable<Issue> includedIssues = _allIssues.Where(issue => !issue.Labels.Any(label => ignoreLabels.Any(ignoreLabel => ignoreLabel.Id == label.Id)));
@@ -175,17 +185,17 @@ namespace GithubIssueTagger
             //263262236       Functionality:VisualStudioUI = PM UI
             Label pmuiLabel = await GetLabelById(263262236);
             IEnumerable<Issue> pmuiIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == pmuiLabel.Id));
-            Console.WriteLine("PM UI\t" + pmuiIssues.Count());
+            Console.WriteLine("PM UI\t" + pmuiIssues.Count() + GetUntypedIssueCountString(pmuiIssues, validTypeLabels));
 
             //430506461       Product: VS.PMConsole = PMC
             Label pmcLabel = await GetLabelById(430506461);
             IEnumerable<Issue> pmcIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == pmcLabel.Id));
-            Console.WriteLine("PMC\t" + pmcIssues.Count());
+            Console.WriteLine("PMC\t" + pmcIssues.Count() + GetUntypedIssueCountString(pmcIssues, validTypeLabels));
 
             //171462728       Functionality: SDK = SDK
             Label sdkLabel = await GetLabelById(171462728);
             IEnumerable<Issue> sdkIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == sdkLabel.Id));
-            Console.WriteLine("SDK\t" + sdkIssues.Count());
+            Console.WriteLine("SDK\t" + sdkIssues.Count() + GetUntypedIssueCountString(sdkIssues, validTypeLabels));
 
             //CLI(nuget, dotnet, msbuild)
             //{
@@ -198,17 +208,17 @@ namespace GithubIssueTagger
             Label cliLabel3 = await GetLabelById(1048477918);
             var cliLabels = new long[] { cliLabel1.Id, cliLabel2.Id, cliLabel3.Id };
             IEnumerable<Issue> cliIssues = includedIssues.Where(issue => issue.Labels.Any(label => cliLabels.Contains(label.Id)));
-            Console.WriteLine("CLI\t" + cliIssues.Count());
+            Console.WriteLine("CLI\t" + cliIssues.Count() + GetUntypedIssueCountString(cliIssues, validTypeLabels));
 
             //330852328       Functionality: Pack = Pack
             Label packLabel = await GetLabelById(330852328);
             IEnumerable<Issue> packIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == packLabel.Id));
-            Console.WriteLine("Pack\t" + packIssues.Count());
+            Console.WriteLine("Pack\t" + packIssues.Count() + GetUntypedIssueCountString(packIssues, validTypeLabels));
 
             //332553843       Functionality: Push = Push
             Label pushLabel = await GetLabelById(332553843);
             IEnumerable<Issue> pushIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == pushLabel.Id));
-            Console.WriteLine("Push\t" + pushIssues.Count());
+            Console.WriteLine("Push\t" + pushIssues.Count() + GetUntypedIssueCountString(pushIssues, validTypeLabels));
 
             //Restore
             //{
@@ -227,12 +237,12 @@ namespace GithubIssueTagger
             Label restoreLabel6 = await GetLabelById(664611674);
             var restoreLabels = new long[] { restoreLabel1.Id, restoreLabel2.Id, restoreLabel3.Id, restoreLabel4.Id, restoreLabel5.Id, restoreLabel6.Id };
             IEnumerable<Issue> restoreIssues = includedIssues.Where(issue => issue.Labels.Any(label => restoreLabels.Contains(label.Id)));
-            Console.WriteLine("Restore\t" + restoreIssues.Count());
+            Console.WriteLine("Restore\t" + restoreIssues.Count() + GetUntypedIssueCountString(restoreIssues, validTypeLabels));
 
             // 702649610       Functionality: Signing = Signing / verification
             Label signingLabel = await GetLabelById(702649610);
             IEnumerable<Issue> signingIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == signingLabel.Id));
-            Console.WriteLine("Signing\t" + signingIssues.Count());
+            Console.WriteLine("Signing\t" + signingIssues.Count() + GetUntypedIssueCountString(signingIssues, validTypeLabels));
 
             //Package Management(install/ uninstall / update)
             //{
@@ -243,7 +253,7 @@ namespace GithubIssueTagger
             Label managementLabel2 = await GetLabelById(336427001);
             var managementLabels = new long[] { managementLabel1.Id, managementLabel2.Id };
             IEnumerable<Issue> managementIssues = includedIssues.Where(issue => issue.Labels.Any(label => managementLabels.Contains(label.Id)));
-            Console.WriteLine("Package Management\t" + managementIssues.Count());
+            Console.WriteLine("Package Management\t" + managementIssues.Count() + GetUntypedIssueCountString(managementIssues, validTypeLabels));
 
             //Search + List
             //{
@@ -254,12 +264,24 @@ namespace GithubIssueTagger
             Label searchListLabel2 = await GetLabelById(723045456);
             var searchListLabels = new long[] { searchListLabel1.Id, searchListLabel2.Id };
             IEnumerable<Issue> searchListIssues = includedIssues.Where(issue => issue.Labels.Any(label => searchListLabels.Contains(label.Id)));
-            Console.WriteLine("Search & List\t" + searchListIssues.Count());
+            Console.WriteLine("Search & List\t" + searchListIssues.Count() + GetUntypedIssueCountString(searchListIssues, validTypeLabels));
 
             //    723058565       Area: NewFrameworks = Core
             Label coreLabel = await GetLabelById(723058565);
             IEnumerable<Issue> coreIssues = includedIssues.Where(issue => issue.Labels.Any(label => label.Id == coreLabel.Id));
-            Console.WriteLine("Core\t" + coreIssues.Count());
+            Console.WriteLine("Core\t" + coreIssues.Count() + GetUntypedIssueCountString(coreIssues, validTypeLabels));
+        }
+
+        private static string GetUntypedIssueCountString(IEnumerable<Issue> issues, List<Label> validTypeLabels)
+        {
+            IEnumerable<long> validTypeLabelIds = validTypeLabels.Select(label => label.Id);
+            IEnumerable<Issue> unlabeledIssues = issues.Where(issue => !issue.Labels.Any(label => validTypeLabelIds.Contains(label.Id)));
+            var unlabeledCount = unlabeledIssues.Count();
+            if (unlabeledCount > 0)
+            {
+                return " (Missing Types: " + unlabeledCount + ")";
+            }
+            return string.Empty;
         }
 
 
