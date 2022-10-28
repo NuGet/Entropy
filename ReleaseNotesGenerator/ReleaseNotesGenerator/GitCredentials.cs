22604e16e146360f778b5f209593f9c94b2cc94f
Nikolche Kolev <nikolev@microsoft.com>
2022-10-27 18:10:11 -0700

    Further cleanup of the release notes generator. Update the namespaces, remove redundant dependencies (#96)

--- ReleaseNotesGenerator/ReleaseNotesGenerator/GitCredentials.cs
+++ ReleaseNotesGenerator/ReleaseNotesGenerator/GitCredentials.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace ChangelogGenerator
+namespace ReleaseNotesGenerator
 {
     internal class GitCredentials
     {
--- ReleaseNotesGenerator/ReleaseNotesGenerator/Program.cs
+++ ReleaseNotesGenerator/ReleaseNotesGenerator/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 
 
-namespace ChangelogGenerator
+namespace ReleaseNotesGenerator
 {
     class Program
     {
