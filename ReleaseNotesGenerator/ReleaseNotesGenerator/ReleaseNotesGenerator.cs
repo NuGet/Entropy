4121b34cd76df436f3600c08d186cf849e8b4a07
Nikolche Kolev <nikolev@microsoft.com>
2022-10-31 14:31:37 -0700

    Improve the release notes generator (#97)
    This PR adds the ability to harvest community contributions automatically. To achieve that we need to provide the start commit. We can start inferring that commit from the tags if we wish. That change would be really simple, but I decided not to make too many changes for now.
    Remove some unnecessary options. We've made parts of this tool generic in the past, but let's be real, it's super tailored to our repo and label management.
    Added "TODO" for everything in the release notes that needs updated after you run the automatic generation.
    Also added a bunch of things that can be inferred automatic, such as VS year, VS versions etc. This requires the occasional update of the tool, but better have to update the tool once every 2 years, then do manual like 8-9 over a few years.

--- ReleaseNotesGenerator/ReleaseNotesGenerator/ReleaseNotesGenerator.cs
+++ ReleaseNotesGenerator/ReleaseNotesGenerator/ReleaseNotesGenerator.cs
@@ -3,12 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ReleaseNotesGenerator
 {
     class ReleaseNotesGenerator
     {
+        private const string NuGet = "nuget";
+        private const string NuGetClient = "nuget.client";
+        private const string Home = "home";
+
         private readonly GitHubClient GitHubClient;
         private readonly Options Options;
 
@@ -24,7 +29,7 @@ namespace ReleaseNotesGenerator
             }
             else
             {
-                Dictionary<string, string> credentuals = GitCredentials.Get(new Uri("https://github.com/NuGet/Home"));
+                Dictionary<string, string> credentuals = GitCredentials.Get(new Uri($"https://github.com/{NuGet}/{Home}"));
                 if (credentuals?.TryGetValue("password", out string pat) == true)
                 {
                     creds = new Credentials(pat);
@@ -39,8 +44,10 @@ namespace ReleaseNotesGenerator
 
         public async Task<string> GenerateChangelog()
         {
-            Dictionary<IssueType, List<Issue>> issues = await GetIssuesByType(Options.Release);
-            return GenerateMarkdown(Options.Release, issues);
+            Dictionary<IssueType, List<Issue>> issues = await GetIssuesByType(NuGet, Home, Options.Release);
+            List<PullRequest> communityPullRequests = await GetCommunityPullRequests(GitHubClient, NuGet, NuGetClient, Options.StartSha, $"release-{Options.Release}.x");
+
+            return GenerateMarkdown(Options.Release, issues, communityPullRequests);
         }
 
         public static async Task<IList<Issue>> GetIssuesForMilestone(GitHubClient client, string org, string repo, Milestone milestone)
@@ -57,11 +64,59 @@ namespace ReleaseNotesGenerator
             return issuesForMilestone.ToList();
         }
 
-        private async Task<Dictionary<IssueType, List<Issue>>> GetIssuesByType(string releaseId)
+        public static async Task<List<PullRequest>> GetCommunityPullRequests(GitHubClient gitHubClient, string orgName, string repoName, string startSha, string branchName, string endSha = null)
+        {
+            Console.Write($"Processing the pull requests for {branchName}, ");
+
+            var githubBranch = await gitHubClient.Repository.Branch.Get(orgName, repoName, branchName);
+            string finalSha = string.IsNullOrEmpty(endSha) ? githubBranch.Commit.Sha : endSha;
+
+            Console.WriteLine($"starting with {startSha} and ending with {finalSha}");
+
+            var commits = (await gitHubClient.Repository.Commit.Compare(orgName, repoName, startSha, finalSha)).Commits;
+
+            List<PullRequest> pullRequests = new();
+
+            foreach (var commit in commits)
+            {
+                var assumedId = GetPRId(commit.Commit.Message);
+                try
+                {
+                    var pullRequest = await gitHubClient.Repository.PullRequest.Get(orgName, repoName, assumedId);
+                    var isCommunity = pullRequest.Labels.Any(e => e.Name == IssueLabels.Community);
+                    if (isCommunity)
+                    {
+                        pullRequests.Add(pullRequest);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"Failed retrieving the pull request for {commit.HtmlUrl}. Calculate PR Id: {assumedId}");
+                }
+            }
+
+            return pullRequests;
+
+            static int GetPRId(string message)
+            {
+                //use RegexOptions.RightToLeft to match from the right side, to ignore the other numbers in the title
+                //E.g. 	Fix spelling of Wiederherstellen (NuGet/Home#11774) (#4591)
+                //Or, Revert "Disable timing out EndToEnd tests (#4592)" (#4597) Fixes https://github.com/NuGet/Client.Engineering/issues/1572 This reverts commit acee7c1c1773e3d96ca806b10ba068dd09b0baf5.
+                foreach (Match match in new Regex(@"\(#\d+\)", RegexOptions.RightToLeft).Matches(message))
+                {
+                    // match={(#4634)}, pullRequestsIdText=4634
+                    var pullRequestIdText = match.Value.Substring(2, match.Length - 3);
+                    int.TryParse(pullRequestIdText, out int prId);
+                    return prId;
+                }
+
+                return -1;
+            }
+        }
+
+        private async Task<Dictionary<IssueType, List<Issue>>> GetIssuesByType(string org, string repo, string releaseId)
         {
             var issuesByType = new Dictionary<IssueType, List<Issue>>();
-
-            GetRepositoryDetrails(out string org, out string repo);
 
             Milestone relevantMilestone = await FindMatchingMilestone(releaseId, org, repo);
 
@@ -159,17 +214,6 @@ namespace ReleaseNotesGenerator
             return issuesByType;
         }
 
-        private void GetRepositoryDetrails(out string org, out string repo)
-        {
-            var repoParts = Options.Repo.Split("/");
-            if (repoParts.Length != 2)
-            {
-                throw new Exception($"Expected the repo to be 2 part, separated by `/`. Repo:{Options.Repo }, parts{string.Join("; ", repoParts)} ");
-            }
-            org = repoParts[0];
-            repo = repoParts[1];
-        }
-
         private async Task<Milestone> FindMatchingMilestone(string releaseId, string org, string repo)
         {
             var milestones = await GitHubClient.Issue.Milestone.GetAllForRepository(org, repo);
@@ -183,46 +227,115 @@ namespace ReleaseNotesGenerator
             return relevantMilestone;
         }
 
-        private string GenerateMarkdown(string releaseId, Dictionary<IssueType, List<Issue>> labelSet)
+        private static string GenerateMarkdown(string release, Dictionary<IssueType, List<Issue>> labelSet, List<PullRequest> communityPullRequests)
         {
+            var version = Version.Parse(release);
+            string VSYear = GetVSYear(version.Major);
+            string VSVersion = GetVSVersion(version);
+            string fullSDKVersion = "<TODO: Full SDK Version>";
+            string SDKMajorMinorVersion = "<TODO: SDKMajorMinorVersionOnly";
+            string GithubAlias = "<TODO: GitHubAlias>";
+            string MicrosoftAlias = "<TODO: MicrosoftAlias>";
             StringBuilder builder = new StringBuilder();
             builder.AppendLine("---");
-            builder.AppendLine(string.Format("title: NuGet {0} Release Notes", Options.Release));
-            builder.AppendLine(string.Format("description: Release notes for NuGet {0} including new features, bug fixes, and DCRs.", Options.Release));
-            builder.AppendLine("author: <GithubAlias>");
-            builder.AppendLine("ms.author: <MicrosoftAlias>");
+            builder.AppendLine(string.Format("title: NuGet {0} Release Notes", release));
+            builder.AppendLine(string.Format("description: Release notes for NuGet {0} including new features, bug fixes, and DCRs.", release));
+            builder.AppendLine(string.Format("author: {0}", GithubAlias));
+            builder.AppendLine(string.Format("ms.author: {0}", MicrosoftAlias));
             builder.AppendLine(string.Format("ms.date: {0}", DateTime.Now.ToString("d", System.Globalization.CultureInfo.GetCultureInfo("en-US"))));
             builder.AppendLine("ms.topic: conceptual");
             builder.AppendLine("---");
             builder.AppendLine();
-            builder.AppendLine(string.Format("# NuGet {0} Release Notes", Options.Release));
+            builder.AppendLine(string.Format("# NuGet {0} Release Notes", release));
             builder.AppendLine();
             builder.AppendLine("NuGet distribution vehicles:");
             builder.AppendLine();
             builder.AppendLine("| NuGet version | Available in Visual Studio version | Available in .NET SDK(s) |");
             builder.AppendLine("|:---|:---|:---|");
-            builder.AppendLine("| [**<NuGetVersion>**](https://nuget.org/downloads) | [Visual Studio <VSYear> version <VSVersion>](https://visualstudio.microsoft.com/downloads/) | [<SDKVersion>](https://dotnet.microsoft.com/download/dotnet-core/<SDKMajorMinorVersionOnly>)<sup>1</sup> |");
+            builder.AppendLine(string.Format("| [**{0}**](https://nuget.org/downloads) |" +
+                " [Visual Studio {1} version {2}](https://visualstudio.microsoft.com/downloads/) " +
+                "| [{3}](https://dotnet.microsoft.com/download/dotnet-core/{4})<sup>1</sup> |",
+                release, VSYear, VSVersion, fullSDKVersion, SDKMajorMinorVersion));
             builder.AppendLine();
-            builder.AppendLine("<sup>1</sup> Installed with Visual Studio <VSYear> with.NET Core workload");
+            builder.AppendLine(string.Format("<sup>1</sup> Installed with Visual Studio {0} with.NET Core workload", VSYear));
             builder.AppendLine();
-            builder.AppendLine(string.Format("## Summary: What's New in {0}", Options.Release));
+            builder.AppendLine(string.Format("## Summary: What's New in {0}", release));
             builder.AppendLine();
             OutputSection(labelSet, builder, IssueType.Feature, includeHeader: false);
             builder.AppendLine("### Issues fixed in this release");
             builder.AppendLine();
             OutputSection(labelSet, builder, IssueType.DCR);
             OutputSection(labelSet, builder, IssueType.Bug);
+            builder.AppendLine("[List of commits in this release](TODO: Provide the link.)");
+            builder.AppendLine();
+            OutputCommunityPullRequestsSection(communityPullRequests, builder);
 
             foreach (var key in labelSet.Keys)
             {
                 if (key != IssueType.Feature && key != IssueType.DCR && key != IssueType.Bug)
                 {
-                    // these sections shouldn't exist. tweak the issues in github until these issues move to Feature, Bug, DCR, or go away.
                     OutputSection(labelSet, builder, key, problem: true);
                 }
             }
 
             return builder.ToString();
+        }
+
+        private static string GetVSVersion(Version version)
+        {
+            return version.Major + 11 + "." + version.Minor;
+        }
+
+        private static string GetVSYear(int major)
+        {
+            Dictionary<int, int> NuGetMajorToVSYearMapping = new()
+            {
+                { 5, 2019 },
+                { 6, 2022 },
+            };
+
+            if (NuGetMajorToVSYearMapping.TryGetValue(major, out int VSYear))
+            {
+                return VSYear.ToString();
+            }
+            return "<TODO: VSYear. Unknown version/year combo, consider updating the tool.>";
+        }
+
+        private static void OutputCommunityPullRequestsSection(
+            List<PullRequest> communityPullRequests,
+            StringBuilder builder)
+        {
+            if (communityPullRequests != null)
+            {
+                if (communityPullRequests.Count > 0)
+                {
+                    AddCommunityContributionsHeader(builder);
+                    builder.AppendLine("Thank you to all the contributors who helped make this NuGet release awesome!");
+                    builder.AppendLine();
+
+                    var contributors = communityPullRequests.GroupBy(e => e.User).OrderBy(e => e.Count());
+                    foreach (var contribution in contributors)
+                    {
+                        builder.AppendLine($"* [{contribution.Key.Login}]({contribution.Key.HtmlUrl})");
+                        foreach (var PR in contribution)
+                        {
+                            builder.AppendLine($"  * [{PR.Number}]({PR.HtmlUrl}) {PR.Title}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                AddCommunityContributionsHeader(builder);
+                builder.AppendLine("TODO: The automatic generation of the release notes did not try to generate the community contributions. " +
+                    "Either rerun the tool with the community contributions option or add the contributors manually. You may delete this section if there were not community contributions.");
+            }
+
+            static void AddCommunityContributionsHeader(StringBuilder builder)
+            {
+                builder.AppendLine("### Community contributions");
+                builder.AppendLine();
+            }
         }
 
         private static void OutputSection(
@@ -232,20 +345,16 @@ namespace ReleaseNotesGenerator
             bool includeHeader = true,
             bool problem = false)
         {
-
-            List<Issue> issues = null;
-            bool hasIssues = labelSet.TryGetValue(key, out issues);
-
-            if (hasIssues)
+            if (labelSet.TryGetValue(key, out List<Issue> issues))
             {
                 if (includeHeader)
                 {
-                    var issueTypeString = key.ToString();
+                    var issueTypeString = (problem ? "TODO: Issues that could not be categorized. Make sure the issue has the correct milestone (if required) or an appropriate Type label - " : string.Empty) + key.ToString();
                     builder.AppendLine(string.Format("**{0}s:**", issueTypeString));
                     builder.AppendLine();
                 }
 
-                foreach (var issue in labelSet[key])
+                foreach (var issue in issues)
                 {
                     builder.AppendLine("* " + issue.Title + " - " + "[#" + issue.Number + "](" + issue.HtmlUrl + ")");
                     builder.AppendLine();
