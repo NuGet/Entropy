e4ba1312ae4fb06fe0251f38b0d8072400959d26
Nikolche Kolev <nikolev@microsoft.com>
2023-06-29 17:18:12 -0700

    Add milestones

--- NuGetReleaseTool/NuGetReleaseTool/GenerateReleaseChangelogCommand/ReleaseChangelogGenerator.cs
+++ NuGetReleaseTool/NuGetReleaseTool/GenerateReleaseChangelogCommand/ReleaseChangelogGenerator.cs
@@ -26,6 +26,35 @@ namespace NuGetReleaseTool.GenerateReleaseChangelogCommand
                 Constants.NuGetClient,
                 issueRepositories: new string[] { "NuGet/Home", "NuGet/Client.Engineering" },
                 githubCommits);
+
+            var homeRepoIssueNumbers = new List<Tuple<int, string>>();
+
+            foreach(var commit in commits)
+            {
+                foreach(var issue in commit.Issues)
+                {
+                    if (issue.Item2.Contains("nuget/home", StringComparison.OrdinalIgnoreCase))
+                    {
+                        homeRepoIssueNumbers.Add(issue);
+                    }
+                }
+            }
+
+            var expectedMilestone = (await GitHubClient.Issue.Milestone.GetAllForRepository("NuGet", "Home")).Single(e => e.Title == Options.Release);
+
+            foreach(var homeIssue in homeRepoIssueNumbers)
+            {
+                var issue = await GitHubClient.Issue.Get("nuget", "home", homeIssue.Item1);
+                if(issue.Milestone?.Title != Options.Release)
+                {
+                    var toUpdate = issue.ToUpdate();
+                    toUpdate.Milestone = expectedMilestone.Number;
+                    await GitHubClient.Issue.Update("Nuget", "home", homeIssue.Item1, toUpdate);
+                }
+
+
+            }
+
             Helpers.SaveAsMarkdown(commits, directory);
         }
 
