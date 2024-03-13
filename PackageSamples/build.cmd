ae8ec86b4f46d31595a88c57c569b4bba084b648
Brian Robbins <brianrob@microsoft.com>
2024-03-13 16:35:26 -0700

    Restore packages to a local directory rather than the global package cache.

--- PackageSamples/build.cmd
+++ PackageSamples/build.cmd
@@ -1,2 +1,3 @@
 msbuild Packages\Packages.sln /p:Configuration=Release
+msbuild Projects\Projects.sln /t:Restore /p:Configuration=Release
 msbuild Projects\Projects.sln /p:Configuration=Release
--- PackageSamples/clean.cmd
+++ PackageSamples/clean.cmd
@@ -1,2 +1,4 @@
 msbuild Packages\Packages.sln /t:clean /p:Configuration=Release
 msbuild Projects\Projects.sln /t:clean /p:Configuration=Release
+rmdir /Q /S NuGetArtifacts
+rmdir /Q /S NuGetArtifactsInstalled
