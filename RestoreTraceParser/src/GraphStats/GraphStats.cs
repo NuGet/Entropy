32e94b1dd86360a5ea960da86b0d9c563d322b73
Brian Robbins <brianrob@microsoft.com>
2024-04-04 07:46:17 -0700

    Custom Trace Parser for Restore EventSource Events (#113)

---
+++ RestoreTraceParser/src/GraphStats/GraphStats.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Microsoft.Diagnostics.Tracing;
+
+namespace RestoreTraceParser
+{
+    public static class GraphStats
+    {
+        // Can be called from Main to generate statistics about the NuGet graph.
+        // Depends on event "Microsoft-NuGet/WalkAsyncTransitivePackage", which is a prototype
+        // event and not present in mainline builds.  The event is of the form:
+        //      Package (string)
+        //      TransitiveHitCount (int)
+        public static void Execute(string[] args)
+        {
+            PackageTable packageTable = new PackageTable();
+
+            string sourceDirectory = args[1];
+            string[] traceFiles = System.IO.Directory.GetFiles(sourceDirectory, "*.etl");
+            bool firstTrace = true;
+            foreach (string traceFile in traceFiles)
+            {
+                if (traceFile.EndsWith(".clrRundown.etl") || traceFile.EndsWith(".kernel.etl"))
+                {
+                    continue;
+                }
+
+                if (firstTrace)
+                {
+                    firstTrace = false;
+                }
+                else
+                {
+                    packageTable.IncrementRunIndex();
+                }
+                Console.WriteLine($"Processing {traceFile}");
+                ProcessTrace(traceFile, packageTable);
+            }
+
+            if (packageTable.AllRunsIdentical())
+            {
+                Console.WriteLine("All runs are identical.");
+            }
+            else
+            {
+                Console.WriteLine("Not all runs are identical.");
+            }
+
+            string outputPath = Path.Combine(sourceDirectory, "packageTable.csv");
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                packageTable.PrintTable(writer);
+            }
+
+            Console.WriteLine($"Results written to {outputPath}.");
+        }
+
+        private static void ProcessTrace(string pathToTrace, PackageTable packageTable)
+        {
+            using (ETWTraceEventSource source = new ETWTraceEventSource(pathToTrace))
+            {
+                source.Dynamic.AddCallbackForProviderEvent("Microsoft-NuGet", "WalkAsyncTransitivePackage", (data) =>
+                {
+                    string package = data.PayloadString(0);
+                    int transitiveHitCount = (int)data.PayloadValue(1);
+
+                    packageTable.IncrementBy(package, transitiveHitCount);
+                });
+
+                source.Process();
+            }
+        }
+    }
+}
---
+++ RestoreTraceParser/src/PhaseTracker/PhaseProject.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Diagnostics.Tracing;
+
+namespace RestoreTraceParser
+{
+    internal class PhaseProject
+    {
+        private double _restoreProjectStartTimeStamp;
+        private double _restoreProjectStopTimeStamp;
+
+        private double _calculateAndWriteDependencySpecStartTimeStamp;
+        private double _calculateAndWriteDependencySpecStopTimeStamp;
+
+        private double _createRestoreGraphStartTimeStamp;
+        private double _createRestoreGraphStopTimeStamp;
+
+        private double _buildAssetsFileStartTimeStamp;
+        private double _buildAssetsFileStopTimeStamp;
+
+        private double _commitAsyncStartTimeStamp;
+        private double _commitAsyncStopTimeStamp;
+
+        private double _writeCacheFileStartTimeStamp;
+        private double _writeCacheFileStopTimeStamp;
+
+        private double _writeDgSpecFileStartTimeStamp;
+        private double _writeDgSpecFileStopTimeStamp;
+
+        private double _writeLockFileStartTimeStamp;
+        private double _writeLockFileStopTimeStamp;
+
+        private double _writePackagesLockFileStartTimeStamp;
+        private double _writePackagesLockFileStopTimeStamp;
+
+        public double RestoreTime
+        {
+            get { return _restoreProjectStopTimeStamp - _restoreProjectStartTimeStamp; }
+        }
+
+        public double CalculateAndWriteDependencySpecTime
+        {
+            get { return _calculateAndWriteDependencySpecStopTimeStamp - _calculateAndWriteDependencySpecStartTimeStamp; }
+        }
+
+        public double CreateRestoreGraphTime
+        {
+            get { return _createRestoreGraphStopTimeStamp - _createRestoreGraphStartTimeStamp; }
+        }
+
+        public double BuildAssetsFileTime
+        {
+            get { return _buildAssetsFileStopTimeStamp - _buildAssetsFileStartTimeStamp; }
+        }
+
+        public double CommitAsyncTime
+        {
+            get { return _commitAsyncStopTimeStamp - _commitAsyncStartTimeStamp; }
+        }
+
+        public double WriteCacheFileTime
+        {
+            get { return _writeCacheFileStopTimeStamp - _writeCacheFileStartTimeStamp; }
+        }
+
+        public double WriteDgSpecFileTime
+        {
+            get { return _writeDgSpecFileStopTimeStamp - _writeDgSpecFileStartTimeStamp; }
+        }
+
+        public double WriteLockFileTime
+        {
+            get { return _writeLockFileStopTimeStamp - _writeLockFileStartTimeStamp; }
+        }
+
+        public double WritePackagesLockFileTime
+        {
+            get { return _writePackagesLockFileStopTimeStamp - _writePackagesLockFileStartTimeStamp; }
+        }
+
+        public void OnRestoreProjectStart(TraceEvent data)
+        {
+            _restoreProjectStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnRestoreProjectStop(TraceEvent data)
+        {
+            _restoreProjectStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCalculateAndWriteDependencySpecStart(TraceEvent data)
+        {
+            _calculateAndWriteDependencySpecStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCalculateAndWriteDependencySpecStop(TraceEvent data)
+        {
+            _calculateAndWriteDependencySpecStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCreateRestoreGraphStart(TraceEvent data)
+        {
+            _createRestoreGraphStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCreateRestoreGraphStop(TraceEvent data)
+        {
+            _createRestoreGraphStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnBuildAssetsFileStart(TraceEvent data)
+        {
+            _buildAssetsFileStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnBuildAssetsFileStop(TraceEvent data)
+        {
+            _buildAssetsFileStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCommitAsyncStart(TraceEvent data)
+        {
+            _commitAsyncStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnCommitAsyncStop(TraceEvent data)
+        {
+            _commitAsyncStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteCacheFileStart(TraceEvent data)
+        {
+            _writeCacheFileStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteCacheFileStop(TraceEvent data)
+        {
+            _writeCacheFileStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteDgSpecFileStart(TraceEvent data)
+        {
+            _writeDgSpecFileStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteDgSpecFileStop(TraceEvent data)
+        {
+            _writeDgSpecFileStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteLockFileStart(TraceEvent data)
+        {
+            _writeLockFileStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWriteLockFileStop(TraceEvent data)
+        {
+            _writeLockFileStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWritePackagesLockFileStart(TraceEvent data)
+        {
+            _writePackagesLockFileStartTimeStamp = data.TimeStampRelativeMSec;
+        }
+
+        public void OnWritePackagesLockFileStop(TraceEvent data)
+        {
+            _writePackagesLockFileStopTimeStamp = data.TimeStampRelativeMSec;
+        }
+    }
+}
