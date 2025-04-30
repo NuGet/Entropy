5722a0704a05423c01d93e439c6bc2e11e1db3a2
Andy Zivkovic <zivkan@users.noreply.github.com>
2025-04-30 10:56:51 +0930

    Make IceBox watcher report more reliable (#122)

--- GithubIssueTagger/GraphQL/GitHubGraphQLClient.cs
+++ GithubIssueTagger/GraphQL/GitHubGraphQLClient.cs
@@ -32,18 +32,74 @@ namespace GithubIssueTagger.GraphQL
 
         public async Task<GraphQLResponse<T>?> SendAsync<T>(GraphQLRequest request)
         {
-            using var httpResponse = await HttpClient.PostAsJsonAsync(requestUri: (string?)null, request);
+            HttpResponseMessage? httpResponse;
 
-            var rateLimitLimit = httpResponse.Headers.GetValues("X-RateLimit-Limit").SingleOrDefault();
-            var rateLimitRemaining = httpResponse.Headers.GetValues("X-RateLimit-Remaining").SingleOrDefault();
-            var rateLimitReset = long.Parse(httpResponse.Headers.GetValues("X-RateLimit-Reset").Single());
-            var resetTime = DateTime.UnixEpoch.AddSeconds(rateLimitReset);
-            Console.WriteLine($"GraphQL HTTP rate-limit = {rateLimitLimit}, remaining = {rateLimitRemaining}, reset = {resetTime:o}");
+            int attempt = 0;
+            int maxAttempts = 5;
+            TimeSpan delay = TimeSpan.FromSeconds(1);
 
-            using var stream = await httpResponse.Content.ReadAsStreamAsync();
-            GraphQLResponse<T>? response = await JsonSerializer.DeserializeAsync<GraphQLResponse<T>>(stream);
+            for (;;)
+            {
+                httpResponse = null;
+                try
+                {
+                    httpResponse = await HttpClient.PostAsJsonAsync(requestUri: (string?)null, request);
 
-            return response;
+                    var rateLimitLimit = httpResponse.Headers.GetValues("X-RateLimit-Limit").SingleOrDefault();
+                    var rateLimitRemaining = httpResponse.Headers.GetValues("X-RateLimit-Remaining").SingleOrDefault();
+                    var rateLimitReset = long.Parse(httpResponse.Headers.GetValues("X-RateLimit-Reset").Single());
+                    var resetTime = DateTime.UnixEpoch.AddSeconds(rateLimitReset);
+                    Console.WriteLine($"GraphQL HTTP rate-limit = {rateLimitLimit}, remaining = {rateLimitRemaining}, reset = {resetTime:o}");
+
+                    using var stream = await httpResponse.Content.ReadAsStreamAsync();
+                    GraphQLResponse<T>? response = await JsonSerializer.DeserializeAsync<GraphQLResponse<T>>(stream);
+
+                    httpResponse.Dispose();
+
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"GraphQL HTTP error: {ex.Message}");
+
+                    TimeSpan retryAfter = delay;
+                    if (httpResponse != null)
+                    {
+                        Console.WriteLine(httpResponse);
+
+                        if (httpResponse.Content.Headers.ContentLength < 2000)
+                        {
+                            var body = await httpResponse.Content.ReadAsStringAsync();
+                            Console.WriteLine(body);
+                        }
+
+                        if (httpResponse.Headers?.RetryAfter?.Date != null)
+                        {
+                            retryAfter = httpResponse.Headers.RetryAfter.Date.Value - DateTime.UtcNow;
+                            if (retryAfter < TimeSpan.Zero)
+                            {
+                                retryAfter = delay;
+                            }
+                        }
+                        else if (httpResponse.Headers?.RetryAfter?.Delta != null)
+                        {
+                            retryAfter = httpResponse.Headers.RetryAfter.Delta.Value;
+                        }
+
+                        httpResponse.Dispose();
+                    }
+
+                    if (attempt >= maxAttempts)
+                    {
+                        throw;
+                    }
+
+                    Console.WriteLine($"GraphQL HTTP retrying in {retryAfter.TotalSeconds} seconds...");
+                    await Task.Delay(retryAfter);
+                    attempt++;
+                    delay += delay;
+                }
+            }
         }
     }
 }
