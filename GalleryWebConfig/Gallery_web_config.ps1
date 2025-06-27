120ba890ef2aa937a78488c1664ffabc592ec592
Andrei Grigorev <angrigor@microsoft.com>
2025-06-26 18:20:33 -0700

    Gallery config < and > conversion

--- GalleryWebConfig/Gallery_web_config.ps1
+++ GalleryWebConfig/Gallery_web_config.ps1
@@ -22,6 +22,8 @@ foreach ($name in ($appSettings | Get-Member -Type NoteProperty | Select-Object 
 {
     $value = $appSettings.$name;
     $value = $value -replace "\$\$",'$$$$$$$$';
+    $value = $value -replace "<","&lt;";
+    $value = $value -replace ">","&gt;";
     $search = "<add(?:\s+)key=`"$name`"(?:\s+)value=`"[^`"]*`"(?:\s*)/>";
     $replace = "<add key=`"$name`" value=`"$value`"/>";
     # Write-Host "Searching for '$search', replacing with '$replace'"
