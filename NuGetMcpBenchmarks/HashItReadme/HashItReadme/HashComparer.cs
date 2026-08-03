b1e07b1394f91097169959d397b2dd56d2e8e6f1
Nigusu-Allehu <59111203+Nigusu-Allehu@users.noreply.github.com>
2026-08-03 16:19:31 -0700

    more bench

---
+++ NuGetMcpBenchmarks/HashItReadme/HashItReadme/HashComparer.cs
@@ -0,0 +1,10 @@
+namespace HashItReadme;
+
+public sealed class HashComparer
+{
+    public bool AreEqual(string a, int x, string b, int y)
+    {
+        // TODO: implement using Contoso.HashIt.
+        throw new NotImplementedException();
+    }
+}
